203407263e166eb68c4a0258cc063d33f04a7b90
Justin Le <justin@jle.im>
2015-07-07 15:36:08 -0700

    initial commit

---
+++ src/Data/MemoTag.hs
@@ -0,0 +1,45 @@
+{-# LANGUAGE RankNTypes #-}
+
+module Data.MemoTag (
+    MemoTag (_mtFunc, _mtValue, _mtResult)
+  , mkMemoTag
+  , mtValue
+  , mtValue'
+  , mtFunc
+  ) where
+
+import Data.Profunctor
+import Data.Functor.Contravariant
+import Control.Comonad
+
+data MemoTag a b = MemoTag { _mtFunc   :: !(a -> b)
+                           , _mtValue  :: !a
+                           , _mtResult :: !b
+                           }
+
+instance Functor (MemoTag a) where
+    fmap g (MemoTag f v r) = MemoTag (g . f) v (g r)
+
+instance Comonad (MemoTag a) where
+    extract = _mtResult
+    duplicate mt@(MemoTag f v _) = MemoTag (mkMemoTag f) v mt
+    extend g mt@(MemoTag f v _)  = MemoTag (g . mkMemoTag f) v (g mt)
+
+mkMemoTag :: (a -> b) -> a -> MemoTag a b
+mkMemoTag f v = MemoTag f v (f v)
+
+-- mtValue :: Lens' (MemoTag a b) a
+mtValue :: Functor f => (a -> f a) -> MemoTag a b -> f (MemoTag a b)
+mtValue g (MemoTag f v _) = mkMemoTag f <$> g v
+
+-- mtValue' :: Iso' (MemoTag a b) a
+mtValue' :: (a -> b) -> forall p f. (Profunctor p, Functor f) => p a (f a) -> p (MemoTag a b) (f (MemoTag a b))
+mtValue' f = dimap _mtValue (mkMemoTag f <$>)
+
+-- mtFunc :: Lens (MemoTag a b) (MemoTag a c) (a -> b) (a -> c)
+mtFunc :: Functor f => ((a -> b) -> f (a -> c)) -> MemoTag a b -> f (MemoTag a c)
+mtFunc g (MemoTag f v _) = flip mkMemoTag v <$> g f
+
+-- mtResult :: Getter (MemoTag a b) b
+mtResult :: (Contravariant f, Functor f) => (b -> f b) -> MemoTag a b -> f (MemoTag a b)
+mtResult g mt@(MemoTag f _ r) = mt <$ g r
